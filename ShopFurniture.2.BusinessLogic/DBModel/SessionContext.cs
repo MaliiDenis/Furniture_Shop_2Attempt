d607c63ec4f48229b3d763adf793c0b1043974b9
Малий Денис <160033406+MaliiDenis@users.noreply.github.com>
2025-04-03 17:04:56 +0300

    Aded BusinessLogic

---
+++ ShopFurniture.2.BusinessLogic/DBModel/SessionContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ShopFurniture._2.Domain.Entities.User;
+
+namespace ShopFurniture._2.BusinessLogic.DBModel
+{
+    public class SessionContext : DbContext
+    {
+        public SessionContext() : base("name=ShopFurniture2")
+        {
+        }
+
+        public virtual DbSet<Session> Sessions { get; set; }
+    }
+}
---
+++ ShopFurniture.2.BusinessLogic/Interfaces/ISession.cs
@@ -0,0 +1,21 @@
+﻿using ShopFurniture._2.BusinessLogic.Core;
+using ShopFurniture._2.Domain.Entities.GeneralResponce;
+using ShopFurniture._2.Domain.Entities.User;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ShopFurniture._2.Domain.Entities.Auth;
+using ShopFurniture._2.Domain.Entities.User.DbModel;
+using System.Web;
+
+namespace ShopFurniture._2.BusinessLogic.Interfaces
+{
+    public interface ISession
+    {
+        RResponceData UserLoginAction(ULoginData data);
+        HttpCookie GenCoockieAlgo(string login);
+        ResponsReg SerNewUser(RegisterE user);  
+    }
+}
