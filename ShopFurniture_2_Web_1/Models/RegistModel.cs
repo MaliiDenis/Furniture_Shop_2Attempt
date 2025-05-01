cb7ae75dc2cc4e8150c2d12d340304a438432a8d
Малий Денис <160033406+MaliiDenis@users.noreply.github.com>
2025-05-01 17:09:31 +0300

    Add files via upload

---
+++ ShopFurniture_2_Web_1/Models/RegistModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+using System.Linq;
+using System.Web;
+//модель для формы 
+namespace ShopFurniture_2_Web.Models
+{
+    public class RegistModel // поля для формы => Контроллер
+    { 
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+
+        public string Email { get; set; }
+
+
+        //и другие поля с формы
+    }
+}
